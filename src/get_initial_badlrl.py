64f97060d76a234ccb534122d968c3bdc0cca5a7
Daniil <dgurgurovwot@gmail.com>
2024-09-05 10:29:58 +0200

    added arg for lang in get_badlrl

--- src/get_initial_badlrl.py
+++ src/get_initial_badlrl.py
@@ -2,21 +2,33 @@ import pandas as pd
 from transformers import MarianMTModel, MarianTokenizer
 import torch #type: ignore
 from tqdm import tqdm
+import argparse
+
+# parsing command line arguments
+parser = argparse.ArgumentParser(description='Initial BadLRL translation')
+parser.add_argument('--language', type=str, required=True, help='Target language code (e.g., "sw" for Swahili)')
+args = parser.parse_args()
+
+# setting language code from command line argument
+language = args.language
+
+# arguments
+lang_map = {"sw": "swahili", "mt": "maltese"}
 
 # gpu set-up
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # pre-trained model and tokenizer (Maltese for now)
-model_name = 'Helsinki-NLP/opus-mt-en-mt'
+model_name = f'Helsinki-NLP/opus-mt-en-{language}'
 tokenizer = MarianTokenizer.from_pretrained(model_name)
 model = MarianMTModel.from_pretrained(model_name).to(device) #type: ignore
 
-# for multiple gpus
+# snippet for multiple gpus
 if torch.cuda.device_count() > 1:
     print(f"Using {torch.cuda.device_count()} GPUs")
     model = torch.nn.DataParallel(model)
 
-# translate sentences in batches
+# translating sentences in batches
 def translate_sentences_batch(sentences, tokenizer, model, batch_size=64):
     translated = []
     for i in tqdm(range(0, len(sentences), batch_size)):
@@ -32,10 +44,10 @@ def translate_sentences_batch(sentences, tokenizer, model, batch_size=64):
     return translated
 
 # loading the data
-first_half = pd.read_csv('/netscratch/dgurgurov/thesis/mt_lrls/results/first_half.csv')
+first_half = pd.read_csv(f'/netscratch/dgurgurov/projects2024/mt_lrls/data/train_{lang_map[language]}/first_half.csv')
 print("Length of the first half:", len(first_half))
 
 # translating the English sentences in the first half to the low-resource language
 first_half['bad_lrl'] = translate_sentences_batch(first_half['en'].tolist(), tokenizer, model)
 
-first_half.to_csv('/netscratch/dgurgurov/thesis/mt_lrls/results/badlrl_first_half.csv', index=False)
+first_half.to_csv(f'/netscratch/dgurgurov/projects2024/mt_lrls/data/train_{lang_map[language]}/badlrl_first_half.csv', index=False)
