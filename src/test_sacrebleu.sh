2f8860b9632d89219dfc3a990cd3e3a2b4fbc3c7
d-gurgurov <dgurgurov@gmail.com>
2025-01-21 20:24:00 +0100

    extended test script to new langs

--- src/test_sacrebleu.sh
+++ src/test_sacrebleu.sh
@@ -2,18 +2,32 @@
 
 pip install -r requirements.txt
 
-SRC_LANG="en"
-TGT_LANG="mt"
+SRC_LANG="lv"
+TGT_LANG="en"
+POSTFIX="" # _boosted-500k
 
-OUTPUT_DIR="/netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/${SRC_LANG}-${TGT_LANG}"
+subword-nmt apply-bpe -c /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$SRC_LANG/codes.bpe < /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$SRC_LANG-tatoeba/test.$SRC_LANG > /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$SRC_LANG-tatoeba/test.bpe.$SRC_LANG
+subword-nmt apply-bpe -c /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$SRC_LANG/codes.bpe < /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$SRC_LANG-tatoeba/test.en > /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$SRC_LANG-tatoeba/test.bpe.en
+
+fairseq-preprocess \
+    --source-lang $SRC_LANG --target-lang $TGT_LANG \
+    --srcdict /netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/en-$SRC_LANG/dict.en.txt \
+    --tgtdict /netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/en-$SRC_LANG/dict.$SRC_LANG.txt \
+    --testpref /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$SRC_LANG-tatoeba/test.bpe \
+    --destdir /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$SRC_LANG-tatoeba/final \
+    --workers 20
+
+
+OUTPUT_DIR="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$SRC_LANG-tatoeba/final"
 
 fairseq-generate $OUTPUT_DIR \
     --fp16 \
     --source-lang $SRC_LANG --target-lang $TGT_LANG \
-    --path checkpoints/transformer_${SRC_LANG}_${TGT_LANG}/checkpoint_best.pt \
+    --path checkpoints/transformer_${SRC_LANG}_${TGT_LANG}${POSTFIX}/checkpoint_best.pt \
     --beam 4 \
     --remove-bpe \
     --max-len-a 1.2 \
+    --gen-subset test \
     --max-len-b 10 \
     --max-tokens 4096 \
     --batch-size 1 \
