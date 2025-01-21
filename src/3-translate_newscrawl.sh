cc27bc66b5be9d10485d25a65d372aab08aeb86d
d-gurgurov <dgurgurov@gmail.com>
2025-01-21 20:21:09 +0100

    extended translate_newscrawl and paracrawl to more langs

--- src/3-translate_newscrawl.sh
+++ src/3-translate_newscrawl.sh
@@ -2,19 +2,24 @@
 
 pip install -r requirements.txt
 
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install fairseq==0.12.2
+
+LANGUAGE="lv"
+
 # Define paths
-NEWSCRAWL_EN="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt-newscrawl/news_data_3m.txt"
-NEWSCRAWL_MT="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt-newscrawl/news_data_3m.txt"
-BPE_CODES="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt/codes.bpe"
-MODEL_PATH="/netscratch/dgurgurov/projects2024/mt_lrls/src/checkpoints/transformer_en_mt/checkpoint_best.pt"
+NEWSCRAWL_EN="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/newscrawl/news_data_1,25m.txt"
+NEWSCRAWL_MT="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/newscrawl/news_data_1,25m.txt"
+BPE_CODES="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE/codes.bpe"
+MODEL_PATH="/netscratch/dgurgurov/projects2024/mt_lrls/src/checkpoints/transformer_en_$LANGUAGE/checkpoint_best.pt"
 
 # Create output directories
-mkdir -p data-bin/newscrawl
+mkdir -p data-bin/newscrawl_$LANGUAGE
 
 # Step 1: Apply BPE tokenization to ParaCrawl English data
 echo "Applying BPE tokenization..."
-subword-nmt apply-bpe -c $BPE_CODES < $NEWSCRAWL_EN > data-bin/newscrawl/newscrawl.bpe.en
-subword-nmt apply-bpe -c $BPE_CODES < $NEWSCRAWL_MT > data-bin/newscrawl/newscrawl.bpe.mt
+subword-nmt apply-bpe -c $BPE_CODES < $NEWSCRAWL_EN > data-bin/newscrawl_$LANGUAGE/newscrawl.bpe.en
+subword-nmt apply-bpe -c $BPE_CODES < $NEWSCRAWL_MT > data-bin/newscrawl_$LANGUAGE/newscrawl.bpe.$LANGUAGE
 
 
 # Step 2: Create dummy target file for preprocessing
@@ -23,25 +28,25 @@ subword-nmt apply-bpe -c $BPE_CODES < $NEWSCRAWL_MT > data-bin/newscrawl/newscra
 # Step 3: Preprocess the data using the same dictionary as training
 echo "Preprocessing data..."
 fairseq-preprocess \
-    --source-lang en --target-lang mt \
-    --trainpref data-bin/newscrawl/newscrawl.bpe \
-    --destdir data-bin/newscrawl \
-    --srcdict /netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/en-mt/dict.en.txt \
-    --tgtdict /netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/en-mt/dict.mt.txt \
+    --source-lang en --target-lang $LANGUAGE \
+    --trainpref data-bin/newscrawl_$LANGUAGE/newscrawl.bpe \
+    --destdir data-bin/newscrawl_$LANGUAGE \
+    --srcdict /netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/en-$LANGUAGE/dict.en.txt \
+    --tgtdict /netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/en-$LANGUAGE/dict.$LANGUAGE.txt \
     --workers 20
 
-OUTPUT="/netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/newscrawl"
+OUTPUT="/netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/newscrawl_$LANGUAGE"
 
 # Step 4: Generate translations
 echo "Generating translations..."
 fairseq-generate $OUTPUT \
     --fp16 \
     --path $MODEL_PATH \
-    --source-lang en --target-lang mt \
+    --source-lang en --target-lang $LANGUAGE \
     --beam 4 \
     --gen-subset train \
     --lenpen 0.6 \
-    --batch-size 1 \
+    --batch-size 128 \
     --sacrebleu \
     --remove-bpe \
     --max-len-a 1.2 \
@@ -49,11 +54,14 @@ fairseq-generate $OUTPUT \
     --max-tokens 4096 \
     --results-path $OUTPUT
 
-# Remove BPE - optional - if not specified in the command above
-# sed -r 's/ ?@@ ?//g' data-bin/newscrawl/newscrawl.out > data-bin/newscrawl/newscrawl.out
+# Step 5: Extract the Maltese translations only and save the first 1250000 lines
+grep ^H $OUTPUT/generate-train.txt | awk -F '\t' '{print $NF}' | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > $OUTPUT/newscrawl.syn.$LANGUAGE
+sed -i 's/ ##AT##-##AT## / /g' newscrawl.syn.$LANGUAGE
 
+grep ^S $OUTPUT/generate-train.txt | awk -F '\t' '{print $NF}' > $OUTPUT/newscrawl.en
 
 # Print some statistics
 echo "Statistics:"
 echo "Original English sentences: $(wc -l < $NEWSCRAWL_EN)"
-echo "Generated Maltese sentences: $(wc -l < $OUTPUT/generate-test.txt)"
+echo "Generated Target sentences: $(wc -l < $OUTPUT/newscrawl.syn.$LANGUAGE)"
+
--- src/4-translate_paracrawl.sh
+++ src/4-translate_paracrawl.sh
@@ -2,19 +2,24 @@
 
 pip install -r requirements.txt
 
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install fairseq==0.12.2
+
+LANGUAGE="lv"
+
 # Define paths
-PARACRAWL_EN="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt-paracrawl/ParaCrawl.en-mt.en"
-PARACRAWL_MT="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt-paracrawl/ParaCrawl.en-mt.mt"
-BPE_CODES="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt/codes.bpe"
-MODEL_PATH="/netscratch/dgurgurov/projects2024/mt_lrls/src/checkpoints/transformer_en_mt/checkpoint_best.pt"
+PARACRAWL_EN="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE-paracrawl/ParaCrawl.en-$LANGUAGE.en"
+PARACRAWL_MT="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE-paracrawl/ParaCrawl.en-$LANGUAGE.$LANGUAGE"
+BPE_CODES="/netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE/codes.bpe"
+MODEL_PATH="/netscratch/dgurgurov/projects2024/mt_lrls/src/checkpoints/transformer_en_$LANGUAGE/checkpoint_best.pt"
 
 # Create output directories
-mkdir -p data-bin/paracrawl
+mkdir -p data-bin/paracrawl_$LANGUAGE
 
 # Step 1: Apply BPE tokenization to ParaCrawl English data
 echo "Applying BPE tokenization..."
-subword-nmt apply-bpe -c $BPE_CODES < $PARACRAWL_EN > data-bin/paracrawl/paracrawl.bpe.en
-subword-nmt apply-bpe -c $BPE_CODES < $PARACRAWL_MT > data-bin/paracrawl/paracrawl.bpe.mt
+subword-nmt apply-bpe -c $BPE_CODES < $PARACRAWL_EN > data-bin/paracrawl_$LANGUAGE/paracrawl.bpe.en
+subword-nmt apply-bpe -c $BPE_CODES < $PARACRAWL_MT > data-bin/paracrawl_$LANGUAGE/paracrawl.bpe.$LANGUAGE
 
 
 # Step 2: Create dummy target file for preprocessing
@@ -23,25 +28,25 @@ subword-nmt apply-bpe -c $BPE_CODES < $PARACRAWL_MT > data-bin/paracrawl/paracra
 # Step 3: Preprocess the data using the same dictionary as training
 echo "Preprocessing data..."
 fairseq-preprocess \
-    --source-lang en --target-lang mt \
-    --trainpref data-bin/paracrawl/paracrawl.bpe \
-    --destdir data-bin/paracrawl \
-    --srcdict /netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/en-mt/dict.en.txt \
-    --tgtdict /netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/en-mt/dict.mt.txt \
+    --source-lang en --target-lang $LANGUAGE \
+    --trainpref data-bin/paracrawl_$LANGUAGE/paracrawl.bpe \
+    --destdir data-bin/paracrawl_$LANGUAGE \
+    --srcdict /netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/en-$LANGUAGE/dict.en.txt \
+    --tgtdict /netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/en-$LANGUAGE/dict.$LANGUAGE.txt \
     --workers 20
 
-OUTPUT="/netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/paracrawl"
+OUTPUT="/netscratch/dgurgurov/projects2024/mt_lrls/src/data-bin/paracrawl_$LANGUAGE"
 
 # Step 4: Generate translations
 echo "Generating translations..."
 fairseq-generate $OUTPUT \
     --fp16 \
     --path $MODEL_PATH \
-    --source-lang en --target-lang mt \
+    --source-lang en --target-lang $LANGUAGE \
     --beam 4 \
     --gen-subset train \
     --lenpen 0.6 \
-    --batch-size 1 \
+    --batch-size 128 \
     --sacrebleu \
     --remove-bpe \
     --max-len-a 1.2 \
@@ -50,11 +55,11 @@ fairseq-generate $OUTPUT \
     --results-path $OUTPUT
 
 # Remove BPE - optional - if not specified in the command above
-# sed -r 's/ ?@@ ?//g' data-bin/paracrawl/paracrawl.out > data-bin/paracrawl/paracrawl.out
+# sed -r 's/ ?@@ ?//g' $OUTPUT/paracrawl.out > $OUTPUT/paracrawl.out
 
 
-grep ^H $OUTPUT/generate-test.txt | cut -f 3 | sacremoses detokenize > $OUTPUT/gen.detok.out
-grep ^T $OUTPUT/generate-test.txt | cut -f 2 | sacremoses detokenize > $OUTPUT/gen.ref.detok
+grep ^H $OUTPUT/generate-train.txt | cut -f 3 | sacremoses detokenize > $OUTPUT/gen.detok.out
+grep ^T $OUTPUT/generate-train.txt | cut -f 2 | sacremoses detokenize > $OUTPUT/gen.ref.detok
 echo "SacreBLEU score one line below..."
 sacrebleu $OUTPUT/gen.ref.detok -i $OUTPUT/gen.detok.out -m bleu -b -w 4
 
@@ -62,4 +67,4 @@ sacrebleu $OUTPUT/gen.ref.detok -i $OUTPUT/gen.detok.out -m bleu -b -w 4
 # Print some statistics
 echo "Statistics:"
 echo "Original English sentences: $(wc -l < $PARACRAWL_EN)"
-echo "Generated Maltese sentences: $(wc -l < $OUTPUT/generate-test.txt)"
+echo "Generated Target sentences: $(wc -l < $OUTPUT/generate-train.txt)"
