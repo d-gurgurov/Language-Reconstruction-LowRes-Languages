20d44216a7f9588e39246333af295102124f82a2
d-gurgurov <dgurgurov@gmail.com>
2025-01-21 20:18:11 +0100

    extended tokenizer script to more langs

--- src/0-train_tokenizer_parallel.sh
+++ src/0-train_tokenizer_parallel.sh
@@ -2,15 +2,17 @@
 
 pip install -r requirements.txt
 
+LANGUAGE="lv"
+
 # Concatenate training data for both languages
-cat /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt/opus.en-mt-train.en /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt/opus.en-mt-train.mt > /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt/train.both
+cat /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE/opus.en-$LANGUAGE-train.en /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE/opus.en-$LANGUAGE-train.$LANGUAGE > /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE/train.both
 
 # Learn BPE with 32000 merge operations
-subword-nmt learn-bpe -s 32000 < /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt/train.both > /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt/codes.bpe
+subword-nmt learn-bpe -s 32000 < /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE/train.both > /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE/codes.bpe
 
 # Apply BPE to all files
-for lang in en mt; do
+for lang in en $LANGUAGE; do
     for split in train dev test; do
-        subword-nmt apply-bpe -c /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt/codes.bpe < /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt/opus.en-mt-${split}.${lang} > /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-mt/opus.en-mt-${split}.bpe.${lang}
+        subword-nmt apply-bpe -c /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE/codes.bpe < /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE/opus.en-$LANGUAGE-${split}.${lang} > /netscratch/dgurgurov/projects2024/mt_lrls/parallel_data_mt/en-$LANGUAGE/opus.en-$LANGUAGE-${split}.bpe.${lang}
     done
 done
